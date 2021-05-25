d34a8d9c9fd42ac6b9b0dd9ec9fccc209f5b271e
AmanRathoreM <83628481+AmanRathoreM@users.noreply.github.com>
2021-05-25 10:48:37 +0530

    Add files via upload

---
+++ C++ Tutorials/C++ CodeWithHarry Tutorials/Tutorials/tutorial_3.cpp
@@ -0,0 +1,25 @@
+// Date 24-05-2021
+#include <iostream>
+using namespace std;
+int main()
+{
+    // This is a Single line comment
+    //todo This is a Single line comment
+    //* This is a Single line comment
+    //? This is a Single line comment
+    //! This is a Single line comment
+
+    // The down part is a variable
+    int integer_variable = 20;
+    // The upper part is a variable
+
+    cout << "hello" << integer_variable + 20;
+    return 0;
+    /*
+    This is a Multi line comment
+    You can write here                                                                      
+    You can write here                                                                      
+    You can write here                                                                      
+    You can write here                                                                      
+    */
+}
