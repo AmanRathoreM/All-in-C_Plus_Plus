eb089af30d0d4a143c8e212e3a06e60001ff0596
AmanRathoreM <83628481+AmanRathoreM@users.noreply.github.com>
2021-05-25 10:50:10 +0530

    Add files via upload

---
+++ C++ Tutorials/C++ CodeWithHarry Tutorials/Tutorials/tutorial_4_1.cpp
@@ -0,0 +1,14 @@
+// Date 24-05-2021
+#include <iostream>
+using namespace std;
+int main()
+{
+    //? int a = 4;
+    //? int b = 5;
+    int a = 4, b = 5;
+    float pi = 3.1428571428571428571428571428571;
+    char character = '@';
+    cout << "This is tutorial 4.\nHere the value of a is " << a << ".\nThe value of b is " << b << ".\nThe value of Pi is " << pi;
+    cout << "\nCharacter is " << character;
+    return 0;
+}
