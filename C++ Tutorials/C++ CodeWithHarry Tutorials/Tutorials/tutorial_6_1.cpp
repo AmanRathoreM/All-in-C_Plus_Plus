96a564916ed494112606cc5466f0b66a8bab2738
AmanRathoreM <83628481+AmanRathoreM@users.noreply.github.com>
2021-05-26 15:28:33 +0530

    Add files via upload

---
+++ C++ Tutorials/C++ CodeWithHarry Tutorials/Tutorials/tutorial_6_1.cpp
@@ -0,0 +1,16 @@
+// Date 25-05-2021
+
+#include <iostream>
+#include "tutorial_6_1__functions__.cpp"
+
+//* The below line of code will produce an error if func1.cpp is not precente in the current directory
+// #include "func1.cpp"
+//* The above line of code will produce an error if func1.cpp is not precente in the current directory
+
+using namespace std;
+
+int main()
+{
+    cout << "Header files in C++";
+    return 0;
+}
