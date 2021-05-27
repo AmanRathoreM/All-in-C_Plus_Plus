89926ac22ec4d45b07289ebe3712c19dcc49a7b7
AmanRathoreM <83628481+AmanRathoreM@users.noreply.github.com>
2021-05-27 12:10:38 +0530

    Add files via upload

---
+++ C++ Tutorials/C++ CodeWithHarry Tutorials/Tutorials/tutorial_6_2_5.cpp
@@ -0,0 +1,27 @@
+// Date 25-05-2021
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num_1, num_2;
+
+    cout << "Please Enter the value of num_1" << endl;
+    cin >> num_1;
+    cout << "Please Enter the value of num_2" << endl;
+    cin >> num_2;
+    cout << endl;
+
+    cout << "Operators in C++" << endl;
+    cout << "Following are the types of C++ Operators" << endl;
+
+    cout << ">>>> Bitwise Operators" << endl;
+    cout << ">>>>>>>> The value of Bitwise's AND \'&\' operator i.e. (" << num_1 << " & " << num_2 << ") is " << (num_1 & num_2) << endl;
+    cout << ">>>>>>>> The value of Bitwise's OR \'|\' operator i.e. (" << num_1 << " | " << num_2 << ") is " << (num_1 | num_2) << endl;
+    cout << ">>>>>>>> The value of Bitwise's NOT \'~\' operator i.e. (~" << num_1 << ") is " << (~num_1) << " and (~" << num_2 << ") is " << (~num_2) << endl;
+    cout << ">>>>>>>> The value of Bitwise's XOR \'^\' operator i.e. (" << num_1 << " ^ " << num_2 << ") is " << (num_1 ^ num_2) << endl;
+    cout << ">>>>>>>> The value of Bitwise's Left Shift \'<<\' operator i.e. (" << num_1 << " << " << num_2 << ") is " << (num_1 << num_2) << endl;
+    cout << ">>>>>>>> The value of Bitwise's Right Shift \'>>\' operator i.e. (" << num_1 << " >> " << num_2 << ") is " << (num_1 >> num_2) << endl;
+    return 0;
+}
