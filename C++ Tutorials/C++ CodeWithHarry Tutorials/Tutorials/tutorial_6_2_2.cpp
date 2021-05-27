e292fc89620ccb89bb02f23cf5fb66f145a9ade3
AmanRathoreM <83628481+AmanRathoreM@users.noreply.github.com>
2021-05-27 12:09:27 +0530

    Add files via upload

---
+++ C++ Tutorials/C++ CodeWithHarry Tutorials/Tutorials/tutorial_6_2_2.cpp
@@ -0,0 +1,22 @@
+// Date 25-05-2021
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    cout << "Operators in C++" << endl;
+    cout << "Following are the types of C++ Operators" << endl;
+
+    cout << ">>>> Assignment Operators in C++" << endl;
+    cout << ">>>>>>>> In C++ Assignment Operators are used to assign values to any variable." << endl;
+    cout << ">>>>>>>> Such as:" << endl;
+    cout << ">>>>>>>> int x = 1;" << endl;
+    cout << ">>>>>>>> char y = 'S';" << endl;
+    cout << ">>>>>>>> Here \'=\' is an Assignment Operator." << endl;
+    //* These operators are used to assign values to any variable
+    int x = 1;
+    char y = 'S';
+    return 0;
+}
