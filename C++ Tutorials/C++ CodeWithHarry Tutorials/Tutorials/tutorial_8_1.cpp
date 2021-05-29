b0057d5bf9a8f01307a3d9b872f7c853013e1c4d
AmanRathoreM <83628481+AmanRathoreM@users.noreply.github.com>
2021-05-29 10:16:16 +0530

    Add files via upload

---
+++ C++ Tutorials/C++ CodeWithHarry Tutorials/Tutorials/tutorial_8_1.cpp
@@ -0,0 +1,18 @@
+// Date 27-05-2021
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    const int variable_8_1 = 77;
+
+    //*the below line of code will give an error
+    // variable_8_1 = 2;
+    //*the above line of code will give an error
+    cout << ">>>> Constants in C++" << endl;
+    cout << ">>>>>>>> To create a constants variable in C++ just write constant before the syntax of declaring a Variable for example: " << endl;
+    cout << ">>>>>>>> const int variable_8_1 = 77;" << endl;
+    return 0;
+}
