e018e9b445b099f00a12c3a7f0ff418c73bcead6
AmanRathoreM <83628481+AmanRathoreM@users.noreply.github.com>
2021-05-30 11:41:51 +0530

    Add files via upload

---
+++ C++ Tutorials/C++ CodeWithHarry Tutorials/Tutorials/tutorial_10_1_3_1_2.cpp
@@ -0,0 +1,17 @@
+// Date 28-05-2021
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 1;
+    //* Note: do while loop will execute atleast once
+    do
+    {
+        cout << i++ << endl;
+    } while (false);
+    //* Note: do while loop will execute atleast once
+    return 0;
+}
