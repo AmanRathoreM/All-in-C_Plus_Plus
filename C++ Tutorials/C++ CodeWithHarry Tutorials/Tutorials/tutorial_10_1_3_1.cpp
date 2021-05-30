1a973866f040c11d441d19c5d563a453f47b5a7a
AmanRathoreM <83628481+AmanRathoreM@users.noreply.github.com>
2021-05-30 11:41:15 +0530

    Add files via upload

---
+++ C++ Tutorials/C++ CodeWithHarry Tutorials/Tutorials/tutorial_10_1_3_1.cpp
@@ -0,0 +1,16 @@
+// Date 28-05-2021
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 1;
+    do
+    {
+        cout << i++ << endl;
+    } while (i <= 100);
+
+    return 0;
+}
