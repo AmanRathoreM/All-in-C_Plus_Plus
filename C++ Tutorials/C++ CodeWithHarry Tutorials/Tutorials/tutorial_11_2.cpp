f2eeb08bbfcfb655757e9a59934037fb413dec4f
AmanRathoreM <83628481+AmanRathoreM@users.noreply.github.com>
2021-05-31 08:58:13 +0530

    Add files via upload

---
+++ C++ Tutorials/C++ CodeWithHarry Tutorials/Tutorials/tutorial_11_2.cpp
@@ -0,0 +1,18 @@
+// Date 29-05-2021
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i <= 100; i++)
+    {
+        if (i == 10)
+        {
+            continue;
+        }
+        cout << "Hello Aman " << i << " time!" << endl;
+    }
+    return 0;
+}
