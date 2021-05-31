b9c16a1d7d42ef828672c62ded6b4b83d3377f13
AmanRathoreM <83628481+AmanRathoreM@users.noreply.github.com>
2021-05-31 09:03:11 +0530

    Add files via upload

---
+++ C++ Tutorials/C++ CodeWithHarry Tutorials/Tutorials/tutorial_13_2_1_quiz.cpp
@@ -0,0 +1,28 @@
+// Date 29-05-2021
+
+//* Make an array of 10 indices and print it using while loop
+//* Make an array of 10 indices and print it using while loop
+//* Make an array of 10 indices and print it using while loop
+//* Make an array of 10 indices and print it using while loop
+//* Make an array of 10 indices and print it using while loop
+//* Make an array of 10 indices and print it using while loop
+//* Make an array of 10 indices and print it using while loop
+//* Make an array of 10 indices and print it using while loop
+//* Make an array of 10 indices and print it using while loop
+//* Make an array of 10 indices and print it using while loop
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int array_1[10] = {12, 3, 45, 7, 6, 12, 87, 34, 45, 23};
+    int i = 0;
+    while (i < 10)
+    {
+
+        cout << "Value of array_1[" << i << "] is " << array_1[i++] << endl;
+    }
+    return 0;
+}
