d717b028cfa6a4ccddf62dedde3604b0ca696777
AmanRathoreM <83628481+AmanRathoreM@users.noreply.github.com>
2021-05-31 08:59:11 +0530

    Add files via upload

---
+++ C++ Tutorials/C++ CodeWithHarry Tutorials/Tutorials/tutorial_13_1_1_1.cpp
@@ -0,0 +1,15 @@
+// Date 29-05-2021
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int array_1[10] = {12, 3, 45, 7, 6, 12, 87, 34, 45, 23};
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "Value of array_1[" << i << "] is " << array_1[i] << endl;
+    }
+    return 0;
+}
